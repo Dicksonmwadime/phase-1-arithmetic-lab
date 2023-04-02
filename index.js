b607bae463de8c0cafa258ab3916c01503321f48
Your User Name <dicksonmwadime2020@gmail.com>
2023-04-02 07:09:50 +0300

    Inital  commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const multiply = 31 * 2
 
-// Write your code here
+const random = Math.floor(Math.random()* 10) +1
+
+const mod = 25 % 7
+
+const max =Math.max(15, 16,17,18,19,20)
+
+// console.log(multiply)
+// console.log(random)
+// console.log(mod)
+// console.log(max)
